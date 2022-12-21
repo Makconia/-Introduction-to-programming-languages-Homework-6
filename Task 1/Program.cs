2c5aae252aec58a7aeb8c04950a0b75a379b59c7
Makconia <makconia@gmail.com>
2022-12-21 21:57:55 +0300

    One commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+ 
+System.Console.WriteLine("Введите число: ");
+int numb = InputData();
+UserNumber(numb);
+
+
+
+ 
+int InputData()
+{
+    int number = Int32.Parse(Console.ReadLine());
+    return number;
+}
+
+ 
+static void UserNumber(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        if (number > 0) count++;
+        System.Console.WriteLine("Введите еще число или '0' для остановки");
+        number = Int32.Parse(Console.ReadLine());
+    }
+    System.Console.WriteLine("Количество чисел больше 0 = " + count);
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = InputNumber("Введите значение b1 = ");
+double k1 = InputNumber("Введите значение k1 = ");
+double b2 = InputNumber("Введите значение b2 = ");
+double k2 = InputNumber("Введите значение k2 = ");
+CalculationXY(out double x, out double y);
+Print(x, y);
+
+
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Некорректное число");
+    }
+    return number;
+}
+
+
+void CalculationXY(out double x, out double y)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+}
+
+
+void Print(double number, double number2)
+{
+    Console.WriteLine($"Точка пересечения: ({number}; {number2})");
+}
